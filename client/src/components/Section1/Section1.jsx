2f096a79f1d3a78aacb37cea118198a03fb0a77b
Jaswinder Singh <jaswinder.4031@gmail.com>
2025-02-21 14:34:35 +0530

    section1

--- client/src/components/Section1/Section1.jsx
+++ client/src/components/Section1/Section1.jsx
@@ -1,10 +1,16 @@
 import style from './index.module.css'
 import React from 'react'
-
+import sym from './../../assets/universal/sym.svg'
 function Section1() {
   return (
-    <div>
-      
+    <div className={style.container}>
+    <div className={style.heading}>
+      <h2>Your Child's Perfect Schedule in Just a Few Clicks</h2>
+      <p>Less Planning, More Living.</p>
+      <button>
+        <img src={sym} alt="sym" />
+      Join the waiting list</button>
+    </div>
     </div>
   )
 }
