fd8783d4d45aceb304481fa51efaadb1fbf78e25
Jaswinder Singh <jaswinder.4031@gmail.com>
2025-02-22 00:30:43 +0530

    cinn

--- client/src/components/Section1/Section1.jsx
+++ client/src/components/Section1/Section1.jsx
@@ -7,9 +7,11 @@ function Section1() {
     <div className={style.heading}>
       <h2>Your Child's Perfect Schedule in Just a Few Clicks</h2>
       <p>Less Planning, More Living.</p>
-      <button>
+      
+      <a href="#button"><button >
         <img src={sym} alt="sym" />
       Join the waiting list</button>
+      </a>
     </div>
     </div>
   )
--- client/src/components/Section5/Section5.jsx
+++ client/src/components/Section5/Section5.jsx
@@ -2,13 +2,14 @@ import style from './index.module.css'
 import React from 'react'
 import icon from './../../assets/universal/sym.svg'
 function Section5() {
+
   return (
     <div className={style.container}>
     <div className={style.header}>
       <h2>Ready to Simplify Your Life?</h2>
       <p>Be the first to know when we launch! Join our waiting list and get exclusive updates and early access to our journey.</p>
       <br/>
-    <div className={style.button}>
+    <div id='button' className={style.button}>
       <button>
       <img src={icon} alt='icon'/>
       Join the waiting list</button>
