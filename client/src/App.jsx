8e74e86935ff8d37b235a7219b4af39bb3a02492
Jaswinder Singh <jaswinder.4031@gmail.com>
2025-02-21 19:41:35 +0530

    commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,14 +3,14 @@ import style from './App.module.css';
 import {Section1,Section2,Section3,Section4,Section5,Section6} from './components';
 function App() {
   return (
-    <div className={style.container}>
-    <div className={style.internal}>
+    <div>
+    <div>
 
-      {/* <Section1/>
+      <Section1/>
       <Section2/>
       <Section3/>
-      <Section4/> */}
-      {/* <Section5/> */}
+      <Section4/>
+      <Section5/>
       <Section6/>
     </div>
 
